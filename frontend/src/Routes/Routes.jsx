6dd1e867715b847bd94caf6c4af6f596a5eb9af9
MohamedAmr23 <amrm96754@gmail.com>
2025-04-08 00:44:59 +0200

    set navbar

--- frontend/src/Routes/Routes.jsx
+++ frontend/src/Routes/Routes.jsx
@@ -1,16 +1,56 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "../pages/MainLayout.jsx";
 import Home from "../pages/Home.jsx";
+import Doctors from "../pages/Doctors.jsx";
+import About from "../pages/About.jsx";
+import Contact from "../pages/Contact.jsx";
+import Login from "../pages/Login.jsx";
+import MyProfile from "../pages/MyProfile.jsx";
+import MyAppointments from "../pages/MyAppointments.jsx";
+import Appointment from "../pages/Appointment.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <MainLayout/>,
     children: [
       {
-        index: 1,
-        element: <Home />,
+        index: true,
+        element: <Home/>,
       },
+      {
+        path: 'doctors',
+        element:<Doctors/>,
+      },
+      {
+        path: 'doctors/:speciality',
+        element:<Doctors/>,
+      },
+      {
+        path: '/about',
+        element: <About/>,
+      },
+      {
+        path: '/contact',
+        element: <Contact/>,
+      },
+      {
+        path: '/login',
+        element: <Login/>,
+      },
+      {
+        path: '/my-profile',
+        element: <MyProfile/>,
+      },
+      {
+        path: '/my-appointments',
+        element: <MyAppointments/>,
+      },
+      {
+        path: '/appointments/:docId',
+        element: <Appointment/>,
+      },
+     
     ],
   },
 ]);
--- frontend/src/pages/MainLayout.jsx
+++ frontend/src/pages/MainLayout.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Home from './Home.jsx'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar.jsx'
 
 const MainLayout = () => {
   return (
     <>
-    <Home/>
+    <Navbar/>
+    <Outlet/>
     </>
   )
 }
